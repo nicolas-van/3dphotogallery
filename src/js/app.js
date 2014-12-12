2ab99a3019cc4346ad94a19b05dd932c2277269f
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2014-12-12 17:03:28 +0100

    wip

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,20 @@
 (function() {
 "use strict";
 
+
     window.PhotoGallery = function PhotoGallery(element) {
         this.element = element;
         this._photos = [];
         this._planes = [];
+        this._columns = 3;
+        this._tileSize = 5;
+        this._tileSpace = [1, 1];
         this.start();
     };
     _.extend(PhotoGallery.prototype, {
         start: function() {
             this._scene = new THREE.Scene();
-            this._camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+            this._camera = new THREE.PerspectiveCamera( 90, 1, 0.1, 1000 );
             if (window.WebGLRenderingContext)
                 this._renderer = new THREE.WebGLRenderer({ alpha: true });
             else
@@ -21,8 +25,10 @@
             $(window).resize(this.updateSize.bind(this));
             this.updateSize();
 
-            this._camera.position.z = 5;
-            this._camera.lookAt(new THREE.Vector3(0, 0, 0));
+            var width = ((this._tileSize * this._columns) + (this._tileSpace[0] * (this._columns + 1)));
+            this._camera.position.x = width / 2;
+            this._camera.position.y = - (width / 2);
+            this._camera.position.z = width * 0.275;
 
             this._updatePhotos();
 
@@ -43,20 +49,27 @@
                 this._scene.remove(el);
             }.bind(this));
             this._planes = [];
-            this._photos.forEach(function(el) {
+            this._photos.forEach(function(el, i) {
                 var texture = THREE.ImageUtils.loadTexture(el, undefined, function() {
                     var w = texture.image.width;
                     var h = texture.image.height;
                     if (w > h) {
-                        h = 5 / (w / h);
-                        w = 5;
+                        h = this._tileSize / (w / h);
+                        w = this._tileSize;
                     } else {
-                        w = 5 * (w / h);
-                        h = 5;
+                        w = this._tileSize * (w / h);
+                        h = this._tileSize;
                     }
                     var material = new THREE.MeshBasicMaterial({map: texture});
                     var geometry = new THREE.PlaneGeometry(w, h);
                     var plane = new THREE.Mesh(geometry, material);
+
+                    var col = i % this._columns;
+                    var row = Math.floor(i / this._columns);
+                    plane.position.z = 0;
+                    plane.position.x = (col * this._tileSize) + (this._tileSize / 2) + (this._tileSpace[0] * (col + 1));
+                    plane.position.y = - ((row * this._tileSize) + (this._tileSize / 2) + (this._tileSpace[1] * (row + 1)));
+
                     this._scene.add(plane);
                     this._planes.push(plane);
                 }.bind(this));
